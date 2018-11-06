520aa2999377c5cfb1da04eedf9d7fadbe7a57e4
Cody Rickman <cody@creativemindstudios.com>
2018-11-06 12:09:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,8 @@
 # Write your code here.
+def badge_maker (attendee)
+  return "Hello, my name is #{attendee}.";
+end
+
 def batch_badge_creator(attendees)
   badges = [];
   attendees.each {|attendee| badges << "Hello, my name is #{attendee}."}
@@ -9,4 +13,13 @@ def assign_rooms (attendees)
   badge_rooms = [];
   attendees.each_with_index {|attendee, index| badge_rooms << "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"}
   return badge_rooms;
+end
+
+def printer(attendees)
+  messages = batch_badge_creator(attendees);
+  room_assignments = assign_rooms(attendees);
+  messages.each_with_index do |message, index|
+    puts message;
+    puts room_assignments[index];
+  end
 end
